6ce95ca9c078702fd76f52ab4697147d980f3940
rwon@corp.drillmap.com <rwon@corp.drillmap.com>
2018-12-21 18:03:54 -0600

    move files to the root of repo

---
+++ build/precache-manifest.87b7fa3e9d1d730b488eeaf4e4bd2319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfa9aa41305ee60d3c75",
+    "url": "/picturepicker/static/js/runtime~main.cfa9aa41.js"
+  },
+  {
+    "revision": "b1d804428b949bf40715",
+    "url": "/picturepicker/static/js/main.b1d80442.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/picturepicker/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b1d804428b949bf40715",
+    "url": "/picturepicker/static/css/main.7a8101f6.chunk.css"
+  },
+  {
+    "revision": "1198049730205cc9c2f6540dd1a23a2c",
+    "url": "/picturepicker/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/picturepicker/precache-manifest.87b7fa3e9d1d730b488eeaf4e4bd2319.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/picturepicker/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});

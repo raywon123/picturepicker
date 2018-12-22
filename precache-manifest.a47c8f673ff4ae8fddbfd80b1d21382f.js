acf8f82be071a450b021a143b25a8da5c0ce4a3d
raywon123 <raymond_won@yahoo.com>
2018-12-21 18:51:57 -0600

    Updates

---
+++ precache-manifest.a47c8f673ff4ae8fddbfd80b1d21382f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfa9aa41305ee60d3c75",
+    "url": "/picturepicker/static/js/runtime~main.cfa9aa41.js"
+  },
+  {
+    "revision": "bf93b537d4cd1cb13c92",
+    "url": "/picturepicker/static/js/main.bf93b537.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/picturepicker/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bf93b537d4cd1cb13c92",
+    "url": "/picturepicker/static/css/main.bdd2a6d6.chunk.css"
+  },
+  {
+    "revision": "528924a18175f0fc2f7f34f8d3d6e752",
+    "url": "/picturepicker/index.html"
+  }
+];

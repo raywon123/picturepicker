15f3eac45fa18dce01c91b337db479bd8f5332aa
raywon123 <raymond_won@yahoo.com>
2018-12-22 01:20:16 -0600

    App.js: working the scoring

--- src/App.js
+++ src/App.js
@@ -3,31 +3,67 @@ import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import emoji from "./emoji.json";
+import emoji2 from "./emoji.json";
 import Counter from "./components/Counter";
 import "./App.css";
 
 class App extends Component {
-  
+
+  // emoji - for rendering
+  // emoji2 - for keeping score
+
   state = {
-    emoji
+    emoji,
+    emoji2,
+    point: 0,
+    highPoint: 0
   };
 
-  removeEmoji = id => {
-    const emoji = this.state.emoji.filter(emoji => emoji.id !== id);
-    this.setState({ emoji });
-  };
 
   addPoint = id => {
-    const emoji = this.state.emoji.filter(emoji => emoji.id !== id);
-    this.setState({ emoji });
+
+    // check to see if the id is in the array
+    const array = this.state.emoji2.filter(e => e.id === id);
+    console.log(array);
+
+    if (array.length === 0) {
+      // match not found
+      console.log("game over");
+      
+      // reset the score
+      const point = 0;
+      this.setState( { point });
+
+      // reset emoji2 array
+      const emoji2 = Array.from(this.state.emoji);
+      this.setState({ emoji2 });
+    }
+    else {
+      // match found
+      // remove the match object from array
+      const emoji2 = this.state.emoji2.filter(emoji => emoji.id !== id);
+      console.log(emoji2);
+      this.setState({ emoji2 });
+
+      // adding score
+      const point = this.state.point + 1;
+      this.setState({ point });
+
+      // saving the highest point
+      if (point >= this.state.highPoint) {
+        const highPoint = point;
+        this.setState({ highPoint });
+      }
+    }
+
   };
 
-  
+
   render() {
     return (
       <Wrapper>
         <Title>Clicky Game!</Title>
-        <Counter point={3} highPoint={12}/>
+        <Counter point={this.state.point} highPoint={this.state.highPoint} />
         {this.state.emoji.map(emoji => (
           <FriendCard
             addPoint={this.addPoint}
